a6a1177c0c036da5b525da2010f4c843c48e9783
Flipped1121 <136137514+Flipped1121@users.noreply.github.com>
2023-06-20 11:14:34 +0800

    1
    1

--- RandomForest/RF.py
+++ RandomForest/RF.py
@@ -1,15 +1,13 @@
 import pandas as pd
-import numpy as np
-from keras.saving.legacy.saved_model.load import metrics
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import GridSearchCV
-from sklearn.decomposition import PCA
-from sklearn.ensemble import RandomForestClassifier
+import joblib
 
 # 获取数据
 data = pd.read_csv("train_10000.csv")
-
 
 # 缺失值处理
 data = data.fillna(data.mean())
@@ -17,25 +15,26 @@ data = data.fillna(data.mean())
 # 是否存在缺失值
 # print(data.isnull().any())
 # print(data)
+
 y = data["label"]
 x = data.iloc[:, 1:-1]
+
 test = pd.read_csv("validate_1000.csv")
 test = test.fillna(test.mean())
 
 y1 = test["label"]
 x1 = test.iloc[:, 1:-1]
 
-
 # 划分数据集
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=22)
-# 1）实例化一个转换器类
-transfer = PCA(n_components=0.95)
-# 2）调用fit_transform 降维处理
-data_new = transfer.fit_transform(data)
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=22)
+# # 1）实例化一个转换器类
+# transfer = PCA(n_components=0.95)
+# # 2）调用fit_transform 降维处理
+# data_new = transfer.fit_transform(data)
 # 3）标准化
 transfer = StandardScaler()
 x_train = transfer.fit_transform(x_train)
-x_test = transfer.transform(x_test)
+x1 = transfer.transform(x1)
 estimator = RandomForestClassifier()
 # 加入网格搜索与交叉验证
 # 参数准备
@@ -43,15 +42,19 @@ param_dict = {"n_estimators": [120, 200, 300, 500, 800, 1200], "max_depth": [5, 
 estimator = GridSearchCV(estimator, param_grid=param_dict, cv=3)
 estimator.fit(x_train, y_train)
 
-# ）模型评估
+# 保存模型
+joblib.dump(estimator, 'FR_model.joblib')
+# 加载模型
+estimator = joblib.load('FR_model.joblib')
+# 模型评估
 # 直接比对真实值和预测值
 y_predict = estimator.predict(x1)
 print("y_predict:\n", y_predict)
 print("随机森林")
-print(metrics.classification_report(y_predict, y1))
+print(classification_report(y_predict, y1))
 
 # 计算准确率
-score = estimator.score(x_test, y_test)
+score = estimator.score(x1, y1)
 print("准确率为：\n", score)
 
 # 最佳参数：best_params_
