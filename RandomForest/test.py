d949f1ac960340d3a9b57d98308161928c1097ff
Flipped1121 <136137514+Flipped1121@users.noreply.github.com>
2023-06-21 11:09:31 +0800

    1
    1

--- RandomForest/test.py
+++ RandomForest/test.py
@@ -4,7 +4,7 @@ from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import StandardScaler
 import joblib
-
+import csv
 # 获取数据
 data = pd.read_csv(r"D:\Python_Data\web\validate_1000.csv")
 
@@ -54,17 +54,40 @@ estimator = joblib.load('FR_model.joblib')
 y_predict = estimator.predict(x1)
 print("y_predict:\n", y_predict)
 print("随机森林")
-print(classification_report(y_predict, y1))
+y_report = classification_report(y_predict, y1)
+print(y_report)
 
 # 计算准确率
 score = estimator.score(x1, y1)
 print("准确率为：\n", score)
 
 # 最佳参数：best_params_
-print("最佳参数：\n", estimator.best_params_)
+best_params_ = estimator.best_params_
+print("最佳参数：\n", best_params_)
 # 最佳结果：best_score_
-print("最佳结果：\n", estimator.best_score_)
+best_score_ = estimator.best_score_
+print("最佳结果：\n", best_score_)
 # 最佳估计器：best_estimator_
-print("最佳估计器:\n", estimator.best_estimator_)
+best_estimator_ = estimator.best_estimator_
+print("最佳估计器:\n", best_estimator_)
 # 交叉验证结果：cv_results_
-print("交叉验证结果:\n", estimator.cv_results_)
+cv_results_ = estimator.cv_results_
+print("交叉验证结果:\n", cv_results_)
+
+file_handle = open('data.txt', mode='w')
+file_handle.write("预测值为：\n")
+file_handle.write(str(y_predict))
+file_handle.write("\n随机森林\n")
+file_handle.write(str(y_report))
+file_handle.write("\n准确率为：")
+file_handle.write(str(score))
+file_handle.write("\n最佳参数为：\n")
+file_handle.write(str(best_params_))
+file_handle.write("\n最佳得分为：")
+file_handle.write(str(best_score_))
+file_handle.write("\n最佳估计器为：")
+file_handle.write(str(best_estimator_))
+file_handle.write("\n交叉验证结果为：\n")
+file_handle.write(str(cv_results_))
+
+
